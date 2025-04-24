9a408c26382eb5f2244bdde4f607d7f99caa9572
marcelo@agenciasolo.com.br <50111485+marceloranieri@users.noreply.github.com>
2025-04-24 15:30:38 -0300

    fix: add type definitions for heroicons and temporary ChatLayout

---
+++ turf-app/components/ChatLayout.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import { HomeIcon, UserIcon, CogIcon, ChatBubbleOvalLeftIcon, ArrowRightOnRectangleIcon, PlusIcon, HashtagIcon } from '@heroicons/react/24/outline';
+
+export default function ChatLayout() {
+  return null;
+} 
