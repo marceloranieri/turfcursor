258b968daab13684f17517fbde4816a49914eb18
marcelo@agenciasolo.com.br <50111485+marceloranieri@users.noreply.github.com>
2025-04-24 14:33:46 -0300

    chore: remove temporary ChatLayout placeholder

--- turf-app/components/ChatLayout.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import { HomeIcon, UserIcon, CogIcon, ChatBubbleOvalLeftIcon, ArrowRightOnRectangleIcon, PlusIcon, HashtagIcon } from '@heroicons/react/24/outline';
-
-export default function ChatLayout() {
-  return null;
-} 
