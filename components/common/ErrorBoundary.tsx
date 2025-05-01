056e3f6145d2f6dc7a78747049fabd3fee8db752
marcelo@agenciasolo.com.br <50111485+marceloranieri@users.noreply.github.com>
2025-04-30 23:23:09 -0300

    fix: update ErrorBoundary export and imports

--- components/common/ErrorBoundary.tsx
+++ components/common/ErrorBoundary.tsx
@@ -11,7 +11,7 @@ interface State {
   hasError: boolean;
 }
 
-export default class ErrorBoundary extends Component<Props, State> {
+export class ErrorBoundary extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = { hasError: false };
